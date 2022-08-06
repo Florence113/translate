5b80411fcdf9a571d764b4331952b1e3757dbe25
Florence113 <florencejacintaa02@gmail.com>
2022-08-06 18:44:38 +0100

    First commit of translation

---
+++ index.js
@@ -0,0 +1,22 @@
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement(
+//         {pageLanguage: 'en', layout:
+//         google.translate.TranslateElement.InlineLayout.HORIZONTAL},
+//         'google_translate_element'
+//     );
+// }
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en', layout:
+        google.translate.TranslateElement.InlineLayout.SIMPLE},
+        'google_translate_element'
+    );
+}
+
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement(
+//         {pageLanguage: 'en'},
+//         'google_translate_element'
+//     );
+// }
