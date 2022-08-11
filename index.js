8acfcbca00c5b9f04be229326c2b7c2fb73d1b29
Florence113 <florencejacintaa02@gmail.com>
2022-08-11 17:28:29 +0100

    Add button to the translate feature

--- index.js
+++ index.js
@@ -6,13 +6,13 @@
 //     );
 // }
 
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement(
-        {pageLanguage: 'en', layout:
-        google.translate.TranslateElement.InlineLayout.SIMPLE},
-        'google_translate_element'
-    );
-}
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement(
+//         {pageLanguage: 'en', layout:
+//         google.translate.TranslateElement.InlineLayout.SIMPLE},
+//         'google_translate_element'
+//     );
+// }
 
 // function googleTranslateElementInit() {
 //     new google.translate.TranslateElement(
@@ -20,3 +20,19 @@ function googleTranslateElementInit() {
 //         'google_translate_element'
 //     );
 // }
+
+
+
+const trans = document.querySelector(".trans")
+
+trans.addEventListener("click", () => {
+    trans.classList.toggle("show")
+})
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement(
+        {pageLanguage: 'en', layout:
+        google.translate.TranslateElement.InlineLayout.SIMPLE},
+        'google_translate_element'
+    );
+}
